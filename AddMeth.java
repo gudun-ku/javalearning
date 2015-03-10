6f7c33bebc502a06c16371da2955fe62845da7fd
gudun-ku <gudun.ku@gmail.com>
2015-03-10 22:23:29 +0300

    AddMeth: comments added to code

--- AddMeth.java
+++ AddMeth.java
@@ -7,15 +7,17 @@ class AddMeth {
 		Vehicle sportcar = new Vehicle();
 		int range1, range2;
 
-		//set the object minivan fields
+		//установим поля объекта minivan	
 		minivan.passengers = 7;
 		minivan.fuelcap = 16;
 		minivan.mpg = 21;
 
+		//установим поля объекта sportcar
 		sportcar.passengers = 2;
 		sportcar.fuelcap = 14;
 		sportcar.mpg = 12;
 
+		//используем методы объектов для печати
 		System.out.print("Minivan can carry " + 
 		minivan.passengers + ". ");
 
